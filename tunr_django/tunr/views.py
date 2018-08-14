d65d507360d7a04138cccecda58e4284c81be3fd
Shahzad <shahzad.khan@generalassemb.ly>
2018-08-14 13:31:02 -0500

    songs and artist page

--- tunr_django/tunr/views.py
+++ tunr_django/tunr/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Artist, Song
+
+def artist_list(request):
+    artists = Artist.objects.all()
+    return render(request, 'tunr/artist_list.html', {'artists': artists})
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'tunr/song_list.html', {'songs': songs})
